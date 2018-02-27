f17ee7f79675e923c7657cb289a9a487bf1e25ae
Toni Bračič <toni.bracic@iskraimpuls.si>
2018-02-27 19:21:25 +0100

    toni

---
+++ i2MFCS.WMS.WCF/Xsd/LinqToXml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace i2MFCS.WMS.WCF.Xsd
+{
+    class LinqToXml
+    {
+    }
+
+}
